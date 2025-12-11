af7dd8e69e8297e031384f1d942e4b9ff706ea11
CodingManoj <CodingManoj@gmail.com>
2025-12-11 14:35:05 -0500

    Provisioning Gitlab Infra With IAM

--- modules/iam/locals.tf
+++ modules/iam/locals.tf
@@ -2,8 +2,18 @@ locals {
   iam_actions = [
     "ec2:DescribeKeyPairs",
     "ec2:StartInstances",
+    "ec2:StopInstances",
+    "ec2:RunInstances",
+    "ec2:RebootInstances",
     "s3:PutObject",
-    "ssmmessages:CreateControlChannel"
+    "s3:GetObject",
+    "s3:GetObjectAcl",
+    "s3:PutObjectAcl",
+    "ssmmessages:CreateControlChannel",
+    "ssmmessages:CreateDataChannel",
+    "logs:CreateLogGroup",
+    "logs:CreateLogStream",
+    "logs:PutLogEvents"
     # Add more actions here
   ]
 }

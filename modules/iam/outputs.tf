71f28a8a6ccb2011ca55094dfa3fed31617bb01e
CodingManoj <CodingManoj@gmail.com>
2025-12-11 15:13:58 -0500

    Provisioning Gitlab Infra With IAM

--- modules/iam/outputs.tf
+++ modules/iam/outputs.tf
@@ -1,7 +1,11 @@
 
-# output "iam_role_name" {
-#   value = aws_iam_role.ec2_instance_role.name
-# }
+output "agent_iam_role_name" {
+  value = aws_iam_role.agent_iam_role_name.name
+}
+
+output "worker_iam_role_name" {
+  value = aws_iam_role.worker_iam_role_name.name
+}
 
 output "agent_iam_instance_profile_name" {
   value = aws_iam_instance_profile.agent_ec2_instance_profile.name
--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,9 @@
-output "iam_role_name" {
-  value = module.iam.iam_role_name
+output "agent_iam_role_name" {
+  value = module.iam.agent_iam_role_name
+}
+
+output "worker_iam_role_name" {
+  value = module.iam.worker_iam_role_name
 }
 
 output "agent_iam_instance_profile_name" {

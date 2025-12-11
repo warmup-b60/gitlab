b8b86ce639f1fdbe2dbda1c53a0690eb34af3ff6
CodingManoj <CodingManoj@gmail.com>
2025-12-11 15:54:03 -0500

    Provisioning Gitlab Infra With IAM

--- variables.tf
+++ variables.tf
@@ -6,4 +6,4 @@ variable "agent_iam_role_name" {}
 variable "agent_iam_actions" {}
 variable "worker_iam_role_name" {}
 variable "worker_iam_actions" {}
-variable "worker_ami_id" {}
+# variable "worker_ami_id" {}

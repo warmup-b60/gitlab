7b78c68d3618c43b2766b8a876bb3e5706e5c60e
CodingManoj <CodingManoj@gmail.com>
2025-12-11 14:26:48 -0500

    Provisioning Gitlab Infra With IAM

--- env/dev/main.tfvars
+++ env/dev/main.tfvars
@@ -10,9 +10,9 @@ instances = {
   }
 }
 
-ami_id                    = "ami-0fcc78c828f981df2"
-env                       = "dev"
-zone_id                   = "Z056682820YKODKLCFUIZ"
-zone_name                 = "roboshop.internal"
-iam_role_name             = "dev-ec2-instance-role"
-iam_instance_profile_name = "dev-ec2-instance-profile"
+ami_id        = "ami-0fcc78c828f981df2"
+env           = "dev"
+zone_id       = "Z056682820YKODKLCFUIZ"
+zone_name     = "roboshop.internal"
+iam_role_name = "dev-ec2-instance-role"
+# iam_instance_profile_name = "dev-ec2-instance-profile"
--- main.tf
+++ main.tf
@@ -1,13 +1,14 @@
 module "ec2" {
   for_each = var.instances
 
-  source        = "./modules/ec2"
-  ami_id        = var.ami_id
-  instance_type = each.value["instance_type"]
-  env           = var.env
-  name          = each.key
-  zone_id       = var.zone_id
-  zone_name     = var.zone_name
+  source                    = "./modules/ec2"
+  ami_id                    = var.ami_id
+  instance_type             = each.value["instance_type"]
+  env                       = var.env
+  name                      = each.key
+  zone_id                   = var.zone_id
+  zone_name                 = var.zone_name
+  iam_instance_profile_name = module.iam.iam_instance_profile_name
 }
 
 

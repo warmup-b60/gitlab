040a60b0e387efdc633f73102f3d3f5ec837443c
CodingManoj <CodingManoj@gmail.com>
2025-12-11 15:46:05 -0500

    Provisioning Gitlab Infra With IAM

--- outputs.tf
+++ outputs.tf
@@ -12,4 +12,9 @@ output "agent_iam_instance_profile_name" {
 
 output "worker_iam_instance_profile_name" {
   value = module.iam.worker_iam_instance_profile_name
-}  
+}
+
+# Worker AMI ID output
+output "worker_ami_id" {
+  value = module.ec2.worker_ami_id
+}

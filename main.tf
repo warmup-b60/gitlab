eb88ee45305e0a5925957948ebd0f869560293fc
CodingManoj <CodingManoj@gmail.com>
2025-12-11 15:11:01 -0500

    Provisioning Gitlab Infra With IAM

--- main.tf
+++ main.tf
@@ -1,15 +1,15 @@
 module "ec2" {
   for_each = var.instances
 
-  source                          = "./modules/ec2"
-  ami_id                          = var.ami_id
-  instance_type                   = each.value["instance_type"]
-  env                             = var.env
-  name                            = each.key
-  zone_id                         = var.zone_id
-  zone_name                       = var.zone_name
-  agent_iam_instance_profile_name = module.iam.agent_iam_instance_profile_name
-  # worker_iam_instance_profile_name = module.iam.worker_iam_instance_profile_name
+  source                           = "./modules/ec2"
+  ami_id                           = var.ami_id
+  instance_type                    = each.value["instance_type"]
+  env                              = var.env
+  name                             = each.key
+  zone_id                          = var.zone_id
+  zone_name                        = var.zone_name
+  agent_iam_instance_profile_name  = module.iam.agent_iam_instance_profile_name
+  worker_iam_instance_profile_name = module.iam.worker_iam_instance_profile_name
 }
 
 
--- modules/iam/outputs.tf
+++ modules/iam/outputs.tf
@@ -4,9 +4,9 @@
 # }
 
 output "agent_iam_instance_profile_name" {
-  value = aws_iam_instance_profile.agent_iam_instance_profile_name.name
+  value = aws_iam_instance_profile.agent_ec2_instance_profile.name
 }
 
 output "worker_iam_instance_profile_name" {
-  value = aws_iam_instance_profile.worker_ec2_instance_profile_name.name
+  value = aws_iam_instance_profile.worker_ec2_instance_profile.name
 }

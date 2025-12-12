a03de901c8bdea8d3735c536e89fb0933ed891fd
CodingManoj <CodingManoj@gmail.com>
2025-12-12 17:21:02 -0500

    Provisioning Gitlab Infra With IAM

--- modules/ec2/securityGroup.tf
+++ modules/ec2/securityGroup.tf
@@ -1,5 +1,5 @@
 resource "aws_security_group" "agent_sg" {
-  name        = "${var.name}-${var.env}-sg"
+  name        = "${var.name}-${var.env}-agent-sg"
   description = "Allow SSH & HTTPS inbound traffic"
 
   ingress {
@@ -18,7 +18,6 @@ resource "aws_security_group" "agent_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-
   egress {
     from_port   = 0
     to_port     = 0
@@ -28,5 +27,38 @@ resource "aws_security_group" "agent_sg" {
 
   tags = {
     Name = "${var.name}-${var.env}-agent-sg"
+  }
+}
+
+# Worker should allow 22 and 443 only from the agent security group
+resource "aws_security_group" "worker_sg" {
+  name        = "${var.name}-${var.env}-worker-sg"
+  description = "Allow SSH & HTTPS inbound traffic"
+
+  ingress {
+    description = "Allows SSH access"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [aws_security_group.agent_sg.id]
+  }
+
+  ingress {
+    description = "Allows App Access"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = [aws_security_group.agent_sg.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.name}-${var.env}-worker-sg"
   }
 }

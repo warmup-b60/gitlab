2e54820dc87423e93f403551cb8f943a1d8d0bc0
CodingManoj <CodingManoj@gmail.com>
2025-12-12 16:42:24 -0500

    Provisioning Gitlab Infra With IAM

--- modules/ec2/securityGroup.tf
+++ modules/ec2/securityGroup.tf
@@ -9,18 +9,18 @@ resource "aws_security_group" "agent_allow_internal_pors" {
 
 resource "aws_vpc_security_group_ingress_rule" "agent_allow_tls_ipv4" {
   security_group_id = aws_security_group.agent_allow_internal_pors.id
-  cidr_ipv4         = aws_vpc.main.cidr_block
+  cidr_ipv4         = ["0.0.0.0/0"]
   from_port         = 443
   ip_protocol       = "tcp"
   to_port           = 443
 }
 
-resource "aws_vpc_security_group_ingress_rule" "agent_allow_tls_ipv6" {
+resource "aws_vpc_security_group_ingress_rule" "agent_allow_ssh_ipv6" {
   security_group_id = aws_security_group.agent_allow_internal_pors.id
-  cidr_ipv6         = aws_vpc.main.ipv6_cidr_block
-  from_port         = 443
+  cidr_ipv4         = ["0.0.0.0/0"]
+  from_port         = 22
   ip_protocol       = "tcp"
-  to_port           = 443
+  to_port           = 22
 }
 
 resource "aws_vpc_security_group_egress_rule" "agent_all_traffic_ipv4" {
